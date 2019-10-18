ec8bb9747d095561fa19d5bc30b430faf6292e97
Emiliano <emiliano.0218@gmail.com>
2019-10-18 09:47:38 -0500

    cambio el collation de las tablas del plugin

--- includes/class-qmaker-activator.php
+++ includes/class-qmaker-activator.php
@@ -47,31 +47,31 @@ class Qmaker_Activator {
 		$slqAnswers = "CREATE TABLE IF NOT EXISTS " . QM_ANSWERS . "(
 			id int(11) NOT NULL AUTO_INCREMENT,
 			numero_respuesta int(11) NOT NULL,
-			nombre_respuesta text COLLATE utf8mb4_spanish_ci NOT NULL,
-			img_respuesta varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+			nombre_respuesta text COLLATE utf8mb4_unicode_ci NOT NULL,
+			img_respuesta varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 			es_correcta smallint(6) NOT NULL COMMENT '1=correcto, 0=incorrecto',
 			question_id int(11) NOT NULL,
 			PRIMARY KEY (id)
-			)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;";
+			)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 		$wpdb->query( $slqAnswers );
 
 		$slqQuestion = "CREATE TABLE IF NOT EXISTS " . QM_QUESTION . "(
 			id int(11) NOT NULL AUTO_INCREMENT,
 			numero_pregunta int(11) NOT NULL,
-			nombre_pregunta text COLLATE utf8mb4_spanish_ci NOT NULL,
+			nombre_pregunta text COLLATE utf8mb4_unicode_ci NOT NULL,
 			tipo_pregunta int(11) NOT NULL,
 			quiz_id int(11) NOT NULL,
 			PRIMARY KEY (id)
-			)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;";
+			)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 		$wpdb->query( $slqQuestion );
 
 		$sqlQuiz = "CREATE TABLE IF NOT EXISTS " . QM_QUIZ . "(
 			id int(11) NOT NULL AUTO_INCREMENT,
-  			nombre_quiz varchar(40) COLLATE utf8mb4_spanish_ci NOT NULL,
-			descripcion varchar(60) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  			nombre_quiz varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+			descripcion varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 			preguntas_total int(11) NOT NULL DEFAULT 0,
 			PRIMARY KEY (id)
-		)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;";
+		)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 		$wpdb->query( $sqlQuiz );
         
        
